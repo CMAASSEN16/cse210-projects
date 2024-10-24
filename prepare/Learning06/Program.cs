41e916d211de81ef585435611f6c69040004d79a
CMAASSEN16 <enterprisemaassen@gmail.com>
2024-10-23 20:21:14 -0700

    Week 06 Learning Program

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Circle o1 = new Circle("Turqoise", 10);
+        shapes.Add(o1);
+
+        Square o2 = new Square("Blue", 6);
+        shapes.Add(o2);
+ 
+        Rectangle o3 = new Rectangle("Purple", 7, 9);
+        shapes.Add(o3);
+
+        foreach (Shape o in shapes)
+        {
+            string color = o.GetColor();
+
+            double area = o.GetArea();
+
+            Console.WriteLine($"The {color} object has an area of {area}.");
+        }
     }
 }
